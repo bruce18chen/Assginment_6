7966db2cc9d9ab129d24893dbc44de667a62be98
Bruce Chen <80359326+bruce18chen@users.noreply.github.com>
2023-07-08 08:44:30 -0400

    Delete blog directory

--- blog/context_processors.py
+++
@@ -1,8 +0,0 @@
-from django.db.models import Count  # Add this line
-from . import models
-from .models import Topic
-
-
-def base_context(request):
-    top_topics = Topic.objects.annotate(post_count=Count('post')).order_by('-post_count')[:5]  # Update this line
-    return {'top_topics': top_topics}
